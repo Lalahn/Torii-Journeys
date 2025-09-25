72ae96daf44b1bd6b2901a2cb795133da1e5f770
Lalahn <lauravalentina200102@gmail.com>
2025-09-25 10:21:59 -0500

    first commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <nav className="header">
+      <li className="header__item">Destinos</li>
+      <li className="header__item logo">Logo</li>
+      <li className="header__item">Cultura</li>
+    </nav>
+  );
+};
+
+export default Header;
